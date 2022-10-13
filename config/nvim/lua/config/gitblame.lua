16e27ee4738e23b694721f798f1d53291e880139
John Dyer <lbrapid@gmail.com>
2022-10-13 11:17:05 -0400

    Customize gitblame format

--- config/nvim/lua/config/gitblame.lua
+++ config/nvim/lua/config/gitblame.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
   local g = vim.g
   g.gitblame_display_virtual_text = 0
-  g.gitblame_message_template = "<author> • <date>"
+  g.gitblame_message_template = "<author> • <date> • <sha>"
 end
 
 return M
